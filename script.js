d46ec770ea0d9e6e3b1fe228b459828f3dc868b3
Mohammadyasar Mansuri <126454357+Mohammadyasar303@users.noreply.github.com>
2025-02-11 11:26:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ function Car(make, model) {
 	this.make=make;
 	this.model=model;
 }
+Car.prototype.getMakeModel=function () {
+	return `${this.make} ${this.model}`;
+}
 
 function SportsCar(make, model, topSpeed) {
 	Car.call(this, make, model);
 	this.topSpeed=topSpeed;
 }
 
+SportsCar.prototype= Object.create(Car.prototype);
+SportsCar.prototype.getTopSpeed=function () {
+	return this.topSpeed;	
+}
 // // create class car-> add properties to it and its methods
 
 // class Car {
